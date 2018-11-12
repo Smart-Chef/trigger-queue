af663e8280b0ab54f6fd72a6180176fd93b034a2
Russell Pier <russellpier@main-10-231-99-5.tamulink.tamu.edu>
2018-11-12 14:00:58 -0600

    Add UDP thermo sensor receiver

--- sensors/thermometer.go
+++ sensors/thermometer.go
@@ -1,26 +1,81 @@
 package sensors
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	// log "github.com/sirupsen/logrus"
+)
 
 var thermometerInstance *Thermometer
 var thermometerOnce sync.Once
 
+func init() {
+	// todo start python driver
+	fmt.Println("")
+
+}
+
 // Scale should be treated as a singleton
 type Thermometer struct {
 	name string
+	addr *net.UDPAddr
+
 	// TODO: add other metadata
 }
 
 // setupThermometer connects to the physical thermometer
 func (Thermometer) setupThermometer() *Thermometer {
 	// TODO: add code to connect to the actual sensor
-	return &Thermometer{"testThermometer"}
+	remoteAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10000")
+	ln, err := net.ListenUDP("udp", remoteAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ln.SetReadBuffer(16)
+	ln.SetWriteBuffer(16)
+	fmt.Printf("Established connection to %s \n", remoteAddr)
+	// log.Printf("Remote UDP address : %s \n", conn.RemoteAddr().String())
+	log.Printf("Local UDP client address : %s \n", ln.LocalAddr().String())
+	defer ln.Close()
+	//todo set up socket
+	return &Thermometer{
+		name: "testThermometer",
+		addr: remoteAddr,
+	}
 }
 
 // GetTemp gets the current temperature value from the thermometer
-func (*Thermometer) GetTemp() float64 {
-	// TODO: get temp from sensor
-	return 250
+func (t *Thermometer) GetTemp() float64 {
+	ln, err := net.ListenUDP("udp", t.addr)
+	temp := make([]byte, 128)
+	var buffer []byte
+	var length = 0
+
+	// for {
+	tempLength, _ := ln.Read(temp)
+	// if err != nil {
+	// 	break
+	// }
+	buffer = temp
+	length = tempLength
+	// }
+
+	// fmt.Println("UDP Server : ", addr)
+	values := strings.Split(string(buffer[:length]), ",")
+	fmt.Println("Received from UDP server : ", values[0]+","+values[1])
+	probe1, err := strconv.Atoi(values[0])
+	if err != nil {
+		fmt.Println("Non-int value received")
+	}
+	value := float64(probe1) / 100.0
+	fmt.Println(value)
+	defer ln.Close()
+
+	return value
 }
 
 // Implement Singleton GetInstance
