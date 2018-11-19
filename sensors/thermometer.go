81726ad64e16946d873b2f80d8f180b1f513cd2b
Russell Pier <russellpier@main-10-231-73-8.tamulink.tamu.edu>
2018-11-18 23:10:38 -0600

    Update thermometer receiver

--- sensors/thermometer.go
+++ sensors/thermometer.go
@@ -3,12 +3,10 @@ package sensors
 import (
 	"fmt"
 	"net"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
 
-	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -16,11 +14,9 @@ var thermometerInstance *Thermometer
 var thermometerOnce sync.Once
 
 func init() {
-	// Load env variables
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// todo start python driver
+	fmt.Println("")
+
 }
 
 // Scale should be treated as a singleton
@@ -33,19 +29,19 @@ type Thermometer struct {
 
 // setupThermometer connects to the physical thermometer
 func (Thermometer) setupThermometer() *Thermometer {
-	remoteAddr, err := net.ResolveUDPAddr("udp", os.Getenv("THERMOMETER_ADDR"))
+	// TODO: add code to connect to the actual sensor
+	remoteAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10000")
 	ln, err := net.ListenUDP("udp", remoteAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ln.SetReadBuffer(16)
 	ln.SetWriteBuffer(16)
-	log.Printf("Established connection to %s \n", remoteAddr)
+	fmt.Printf("Established connection to %s \n", remoteAddr)
+	// log.Printf("Remote UDP address : %s \n", conn.RemoteAddr().String())
 	log.Printf("Local UDP client address : %s \n", ln.LocalAddr().String())
 	defer ln.Close()
-
-	// TODO: set up socket
+	//todo set up socket
 	return &Thermometer{
 		name: "testThermometer",
 		addr: remoteAddr,
@@ -54,24 +50,45 @@ func (Thermometer) setupThermometer() *Thermometer {
 
 // GetTemp gets the current temperature value from the thermometer
 func (t *Thermometer) GetTemp() float64 {
-	ln, err := net.ListenUDP("udp", t.addr)
+	remoteAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10000")
+	ln, err := net.ListenUDP("udp", remoteAddr)
+
+	if err != nil {
+		log.Error(err.Error())
+	}
 	temp := make([]byte, 128)
 	var buffer []byte
 	var length = 0
 
-	tempLength, _ := ln.Read(temp)
+	// for {
+	tempLength, err := ln.Read(temp)
+
+	if err != nil {
+		log.Error(err.Error())
+	}
+	// if err != nil {
+	// 	break
+	// }
 	buffer = temp
 	length = tempLength
+	// }
 
+	// fmt.Println("UDP Server : ", addr)
 	values := strings.Split(string(buffer[:length]), ",")
 	fmt.Println("Received from UDP server : ", values[0]+","+values[1])
 	probe1, err := strconv.Atoi(values[0])
 	if err != nil {
-		fmt.Println("Non-int value received")
+		log.Error("Non-int value received")
 	}
 	value := float64(probe1) / 100.0
-	fmt.Println(value)
 	defer ln.Close()
+
+	if value == 330.36 {
+		log.Error("Received Null temperature, disregarding")
+		return 0
+	}
+
+	// fmt.Println(value)
 
 	return value
 }
